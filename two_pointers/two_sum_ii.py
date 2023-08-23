0d9ad3a50c0f3913d3c654e544c7d6f0d4dfffef
Lawjay <lawjaylee1@icloud.com>
2023-08-23 12:22:13 -0700

    changed something about two sum

--- two_pointers/two_sum_ii.py
+++ two_pointers/two_sum_ii.py
@@ -4,9 +4,9 @@ class Solution:
         end_pointer = len(numbers) - 1
         
         while beg_pointer < end_pointer:
-            if numbers[beg_pointer] + numbers[end_pointer] == target:
-                return [beg_pointer + 1, end_pointer + 1]
-            elif numbers[beg_pointer] + numbers[end_pointer] > target:
+            if numbers[beg_pointer] + numbers[end_pointer] > target:
                 end_pointer -= 1
             elif numbers[beg_pointer] + numbers[end_pointer] < target:
                 beg_pointer += 1
+            elif numbers[beg_pointer] + numbers[end_pointer] == target:
+                return [beg_pointer + 1, end_pointer + 1]
