4d4b71b65ca44286af7fd1527413edede1d30494
Lawjay <lawjaylee1@icloud.com>
2023-08-22 17:17:30 -0700

    started at school

---
+++ two_pointers/valid_palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if s % 2 != 0
+        beg_pointer = 0
+        end_pointer = len(s) - 1
+
+        for i in range(len(s) / 2):
+            
