afee16883d4ead4eb740fcb502abe28e5285c16a
Lawjay <lawjaylee1@icloud.com>
2023-08-30 12:06:20 -0700

    finished eval_rpn

---
+++ stack/eval_rpn.py
@@ -0,0 +1,29 @@
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+        operators = '+-*/'
+        stack = []
+
+        for token in tokens:
+            if token not in operators:
+                stack.append(int(token))
+                continue
+
+            num_2 = stack.pop()
+            num_1 = stack.pop()
+
+            match token:
+
+                case '+':
+                    stack.append(num_1 + num_2)
+
+                case '-':
+                    stack.append(num_1 - num_2)
+
+                case '*':
+                    stack.append(num_1 * num_2)
+
+                case '/':
+                    stack.append(int(num_1 / num_2))
+
+        return stack.pop()
+
