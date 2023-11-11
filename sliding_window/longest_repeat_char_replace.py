9eda2f1cfa0c00cfb86394287c65ea1c0241cdab
Lawjay <lawjaylee1@icloud.com>
2023-11-10 22:20:17 -0800

    did my own unoptimized answer for longest repeat char replace

---
+++ sliding_window/longest_repeat_char_replace.py
@@ -0,0 +1,18 @@
+# Brute Force
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # we want to get the longest substring where there are k-1 duplicates?
+        left = 0
+        seen = {}
+        max_len = 0
+
+        for i in range(len(s)):
+            seen[s[i]] = seen.get(s[i], 0) + 1
+
+            while i - left + 1 - max(seen.values()) > k:
+                seen[s[left]] = seen.get(s[left], 0) - 1
+                left += 1
+
+            max_len = max(max_len, i - left + 1)
+
+        return max_len
