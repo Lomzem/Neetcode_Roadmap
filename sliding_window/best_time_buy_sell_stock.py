3085372122d213bb17b56e4c43a825959e8ffdc4
Lawjay <lawjaylee1@icloud.com>
2023-11-10 21:44:39 -0800

    other commits?

--- sliding_window/best_time_buy_sell_stock.py
+++ sliding_window/best_time_buy_sell_stock.py
@@ -2,6 +2,7 @@ class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         profit = 0
         lowest = prices[0]
+
         for price in prices:
             lowest = min(lowest, price)
             profit = max(profit, price - lowest)
