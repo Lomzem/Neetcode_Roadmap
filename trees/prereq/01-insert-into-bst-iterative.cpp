ad51770cfcb28e86746198f23bf8dd5293b88955
Lawjay Lee <lawjaylee1@icloud.com>
2024-08-14 15:57:34 -0700

    finish inorder recursive

--- trees/prereq/01-insert-into-bst-iterative.cpp
+++ trees/prereq/01-insert-into-bst-iterative.cpp
@@ -1,24 +1,24 @@
 struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {
     }
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
     }
-    TreeNode(int x, TreeNode *left, TreeNode *right)
+    TreeNode(int x, TreeNode* left, TreeNode* right)
         : val(x), left(left), right(right) {
     }
 };
 
 class Solution {
   public:
-    TreeNode *insertIntoBST(TreeNode *root, int val) {
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
         if (root == nullptr) {
             return new TreeNode(val);
         }
 
-        TreeNode *cur(root);
+        TreeNode* cur(root);
         while (true) {
             if (val < cur->val) {
                 if (cur->left == nullptr) {
---
+++ trees/prereq/03-inorder-traverse-recursive.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {
+    }
+};
+
+class Solution {
+    void inorder(TreeNode* subroot, vector<int>& result) {
+        if (subroot == nullptr) {
+            return;
+        }
+
+        inorder(subroot->left, result);
+        result.push_back(subroot->val);
+        inorder(subroot->right, result);
+    }
+
+  public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorder(root, result);
+
+        return result;
+    }
+};
